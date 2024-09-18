cf63e752ae04ed46da5dd19db4de6633f339fe4c
Anton Panchenko <apanchenko@gmail.com>
2024-09-18 21:06:46 +0300

    Edit name

--- src/cv/api.py
+++ src/cv/api.py
@@ -1,8 +1,10 @@
-from fastapi import Depends, Request, Response
+from typing import Annotated
+from fastapi import Depends, Request, Response, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from playwright.async_api import Page
 from secrets import token_urlsafe
+import jinja_partials
 
 from cv.app import create_application
 from cv.deps import get_db, get_page
@@ -11,6 +13,7 @@ from prisma import Prisma
 app = create_application()
 
 templates = Jinja2Templates(directory='src/templates')
+jinja_partials.register_starlette_extensions(templates)
 
 @app.get('/')
 async def home_page(
@@ -28,7 +31,7 @@ async def home_page(
     return templates.TemplateResponse('index.html', {
         'request': request,
         'include_header': header,
-        **author.model_dump(),
+        'author': author.model_dump(),
     })
 
 
@@ -50,4 +53,54 @@ async def generate(
     )
     return Response(
         headers={'HX-Redirect': f'/static/{name}/anton.pdf'},
+    )
+
+
+@app.get(
+    '/edit/name/{author_id}',
+    response_class=HTMLResponse,
+)
+async def edit_name(
+    request: Request,
+    author_id: int,
+    db: Prisma = Depends(get_db),
+) -> HTMLResponse:
+    """Get edit name form."""
+    author = await db.author.find_first()
+    return templates.TemplateResponse(
+        request=request,
+        name='author_name_edit.html',
+        context={
+            'author_id': author.id,
+            'author_name': author.name,
+        },
+    )
+
+
+@app.put(
+    '/edit/name/{author_id}',
+    response_class=HTMLResponse,
+)
+async def edit_name(
+    request: Request,
+    author_id: int,
+    author_name: Annotated[str, Form()],
+    db: Prisma = Depends(get_db),
+) -> HTMLResponse:
+    """Get edit name form."""
+    author = await db.author.update(
+        where = {
+            'id': author_id
+        },
+        data = {
+            'name': author_name,
+        },
+    )
+    return templates.TemplateResponse(
+        request=request,
+        name='author_name.html',
+        context={
+            'author_id': author.id,
+            'author_name': author.name,
+        },
     )
